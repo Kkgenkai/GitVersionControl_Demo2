c04852cbc9aa0af9dd68702dd817b7fd925100ac
kkgenkai <abenirecovery@gmail.com>
2024-10-13 12:23:37 +0300

    buid_v 1.0.1: mul and div features added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-//calculate app: version 1.0.0
+//calculate app: version 1.0.1
 /* features:
  add
  sub
+ mul
+ div
 */
 //add feature
 let add = (num1, num2)=>{
@@ -12,5 +14,15 @@ let sub = (num1, num2)=>{
     return num1 - num2;
 }
 
+let mul = (num1, num2)=>{
+    return num1 * num2;
+}
+
+let div = (num1, num2)=>{
+    return num1 / num2;
+}
+
 console.log("Add: " + add(2,3));
 console.log("sub: " + sub(2,3));
+console.log("Mul: " + mul(2,3));
+console.log("Div: " + div(2,3));
