bde79d60c566fc45881f8f935e66d0cb993b765e
kkgenkai <abenirecovery@gmail.com>
2024-10-13 12:22:19 +0300

    build_v 1.0.0: add and sub features only

---
+++ app.js
@@ -0,0 +1,16 @@
+//calculate app: version 1.0.0
+/* features:
+ add
+ sub
+*/
+//add feature
+let add = (num1, num2)=>{
+    return num1 + num2;
+}
+
+let sub = (num1, num2)=>{
+    return num1 - num2;
+}
+
+console.log("Add: " + add(2,3));
+console.log("sub: " + sub(2,3));
