e9080b3c576387df659f36d15b31d9a9a5860b39
kkgenkai <abenirecovery@gmail.com>
2024-10-13 13:06:50 +0300

    build_v 1.0.2: disp feature added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//calculate app: version 1.0.1
+//calculate app: version 1.0.2
 /* features:
  add
  sub
  mul
  div
+ display
 */
 //add feature
 let add = (num1, num2)=>{
@@ -22,7 +23,11 @@ let div = (num1, num2)=>{
     return num1 / num2;
 }
 
-console.log("Add: " + add(2,3));
-console.log("sub: " + sub(2,3));
-console.log("Mul: " + mul(2,3));
-console.log("Div: " + div(2,3));
+let disp = ()=>{
+    console.log("Add: " + add(2,3));
+    console.log("sub: " + sub(2,3));
+    console.log("Mul: " + mul(2,3));
+    console.log("Div: " + div(2,3));
+}
+
+disp();
